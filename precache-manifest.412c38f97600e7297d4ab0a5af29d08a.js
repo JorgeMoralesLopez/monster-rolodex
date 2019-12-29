eb6431303ba5fde401ae1d7314cd1fd605ff323f
jorge@jorge-PE60-6QE <moraleslopezjorge@gmail.com>
2019-12-29 18:32:05 +0100

    Updates

---
+++ precache-manifest.412c38f97600e7297d4ab0a5af29d08a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0da10c4cf16058c4a98e29ec044c9532",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "c4080e7df58f9b85dc57",
+    "url": "/monster-rolodex/static/css/main.bd9c6196.chunk.css"
+  },
+  {
+    "revision": "f0f0fb0ad5fd44453858",
+    "url": "/monster-rolodex/static/js/2.ec6f5de8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.ec6f5de8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c4080e7df58f9b85dc57",
+    "url": "/monster-rolodex/static/js/main.71eebfb3.chunk.js"
+  },
+  {
+    "revision": "7e0c8c4379422ca41b5e",
+    "url": "/monster-rolodex/static/js/runtime-main.9ec678d3.js"
+  }
+]);
